5fae22d68a01a90bb8cdc90b37eb16e3a9002d2e
FrancoJZini <francojzini@gail.com>
2023-03-30 13:20:48 -0300

    adding style

--- app/Views/plantilla.php
+++ app/Views/plantilla.php
@@ -9,8 +9,8 @@
     <title>Bootstrap Demo</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia debitis unde dignissimos dolore illum quas ipsam, quos consequuntur obcaecati, earum, et odio voluptatum placeat rem numquam possimus! Doloribus, eius aliquid?</p>
+    <h1 class="p-3 text-primary-emphasis">Hello World!</h1>
+    <p class="dropdown-item active">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia debitis unde dignissimos dolore illum quas ipsam, quos consequuntur obcaecati, earum, et odio voluptatum placeat rem numquam possimus! Doloribus, eius aliquid?</p>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </body>
