fa6a98302b8e0108f4adf9fac6a1c324aac7c5b2
protos50 <francojzini@gmail.com>
2023-04-13 17:32:37 -0300

    carousel and footer completed. Navbar issues solved

--- app/Views/principal.php
+++ app/Views/principal.php
@@ -7,15 +7,18 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Smart Home Corrientes</title>
 
-  <link rel="stylesheet" href="assets/css/styles.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- Include CSS file -->
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <!-- Include Font Awesome CSS file -->
+  <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="/codeigniter_project_ZiniFranco">
         <i class="navbar-brandLogo">
           <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
             <path d="M20.1 14.56a2.07 2.07 0 0 0-.47-.18V9.62a1.64 1.64 0 0 0 .48-.18 1.78 1.78 0 0 0-1.78-3.09 1.62 1.62 0 0 0-.41.32l-4.11-2.38a1.7 1.7 0 0 0 .07-.51 1.78 1.78 0 0 0-3.56 0 1.7 1.7 0 0 0 .07.51L6.28 6.66a1.58 1.58 0 0 0-.41-.31 1.78 1.78 0 0 0-1.78 3.09 1.64 1.64 0 0 0 .48.18v4.76a2.07 2.07 0 0 0-.47.18 1.78 1.78 0 1 0 1.78 3.09 1.72 1.72 0 0 0 .4-.31l4.11 2.37a1.7 1.7 0 0 0-.07.51 1.78 1.78 0 0 0 3.56 0 1.69 1.69 0 0 0-.09-.56l4.09-2.36a1.7 1.7 0 0 0 .44.35 1.78 1.78 0 1 0 1.78-3.09zM6.72 15.69a1.72 1.72 0 0 0-.19-.47 1.53 1.53 0 0 0-.31-.4l5.38-9.33a1.82 1.82 0 0 0 1 0l5.4 9.33a1.53 1.53 0 0 0-.31.4 1.72 1.72 0 0 0-.19.47zM17.5 7.4a1.81 1.81 0 0 0 .17 1.38 1.75 1.75 0 0 0 1.12.84v4.76h-.07l-5.39-9.31.05-.07zM10.82 5a.12.12 0 0 0 0 .05L5.48 14.4h-.07V9.62a1.75 1.75 0 0 0 1.12-.84A1.81 1.81 0 0 0 6.7 7.4zm2.6 14a1.78 1.78 0 0 0-1.32-.58 1.75 1.75 0 0 0-1.28.54L6.7 16.6v-.06h10.78v.11z"></path>
@@ -28,7 +31,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
 
-          <li class="nav-item dropdown position-static">
+          <li class="nav-item dropdown position-static m-0  p-2">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Productos </a>
 
@@ -66,50 +69,143 @@
               </div>
             </ul>
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">Quienes Somos</a>
+          <li class="nav-item  p-2">
+            <a class="nav-link" href="/codeigniter_project_ZiniFranco/nosotros">Quienes Somos</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Comercialización</a>
+          <li class="nav-item  p-2">
+            <a class="nav-link" href="/codeigniter_project_ZiniFranco/catalogo">Comercialización</a>
           </li>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Información de Contacto</a>
+          <li class="nav-item  p-2">
+            <a class="nav-link" href="/codeigniter_project_ZiniFranco/contacto">Información de Contacto</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Términos y Usos</a>
+          <li class="nav-item  p-2">
+            <a class="nav-link" href="/codeigniter_project_ZiniFranco/terminos">Términos y Usos</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 
-  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div id="carouselExampleFade" class="carousel slide carousel-fade">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
+
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="assets/img/img1.jpg" class="d-block w-100 img-fluid" alt="..">
+        <img src="assets/img/img1.jpg" class="d-block w-100 img-fluid" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="assets/img/imgx" class="d-block w-100 img-fluid" alt="..">
+        <img src="assets/img/img2.webp" class="d-block w-100 img-fluid" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="assets/img/imgx" class="d-block w-100 img-fluid" alt="..">
+        <img src="assets/img/img3.jpg" class="d-block w-100 img-fluid" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/img4.avif" class="d-block w-100 img-fluid" alt="...">
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
   </div>
+  >
+
+<!-- body -->
+<section>
+  
+  <p class="text-justify">Bienvenidos a nuestra tienda en línea de productos inteligentes para el hogar. Somos un equipo apasionado por la tecnología y la comodidad en el hogar, y nos dedicamos a ofrecer productos de alta calidad que facilitan la vida cotidiana y mejoran la eficiencia en el hogar.
+    
+    Nuestra misión es ofrecer soluciones innovadoras que permitan a nuestros clientes controlar y monitorear sus hogares de manera inteligente y eficiente. Trabajamos con marcas líderes en la industria para garantizar que nuestros productos sean confiables y de última generación.
+    
+    Nos enorgullece ofrecer un servicio al cliente excepcional y estamos siempre dispuestos a ayudar a nuestros clientes a encontrar el producto adecuado para sus necesidades. Estamos comprometidos a proporcionar una experiencia de compra en línea fácil y segura, con envíos rápidos y seguimiento de pedidos para garantizar la satisfacción del cliente.
+    
+    Gracias por visitar nuestra tienda en línea. Esperamos que encuentres los productos inteligentes para el hogar que estás buscando y que te conviertas en uno de nuestros clientes satisfechos.</p>
+    
+  <p class="text-justify">Nuestra empresa de venta de productos inteligentes para el hogar fue fundada en la región hace más de una década con la visión de mejorar la vida de las personas a través de la tecnología innovadora. Desde entonces, hemos trabajado arduamente para ser líderes en el mercado de productos inteligentes para el hogar, ofreciendo a nuestros clientes una amplia variedad de productos y soluciones que les permiten simplificar sus tareas cotidianas y mejorar su calidad de vida.
+
+A lo largo de nuestra trayectoria, hemos obtenido varios reconocimientos y logros en la región. Hemos sido galardonados con el premio al mejor servicio al cliente en nuestra categoría y hemos sido reconocidos por nuestra labor en el desarrollo de tecnologías sustentables para el hogar. Nos enorgullece saber que nuestros clientes confían en nosotros y en nuestros productos y que hemos logrado impactar positivamente en la vida de muchas personas en la región.</p>  
+    
+  </section>
+
+
+
+
+
+
+
+
+
+
+  <!-- Footer -->
+  <footer class="bg-dark text-light">
+    <div class="container py-4">
+      <div class="row w-100">
+        <div class="col-sm-6 col-md-12 col-lg-3 col-xl-3 mx-auto mt-1">
+          <h5>Sobre Nosotros</h5>
+          <p class="text-justify">Smar Home Corrientes es pionero en la región por brindar soluciones inteligentes para el hogar y ha sabido ganado el reconocimiento por su innovación y calidad. Nos enorgullece haber
+             logrado la satisfacción de nuestros clientes y seguir mejorando cada día para brindarles una experiencia única.</p>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-2 col-xl-2 mx-auto mt-1">
+          <h5>Links Utiles</h5>
+          <ul class="list-unstyled">
+            <li><a href="/codeigniter_project_ZiniFranco">Inicio</a></li>
+            <li><a href="/codeigniter_project_ZiniFranco/catalogo">Catálogo</a></li>
+            <li><a href="/codeigniter_project_ZiniFranco/nosotros">Sobre Nosotros</a></li>
+            <li><a href="/codeigniter_project_ZiniFranco/contacto">Contáctanos</a></li>
+          </ul>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 mx-auto mt-1">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contacto</h6>
+          <p><i class="fas fa-home mr-3"></i> Corrientes, Calle Sim 1560 - Barrio 1, Argentina</p>
+          <p><i class="fas fa-envelope mr-3"></i> smartH_ctes@gmail.com</p>
+          <p><i class="fas fa-phone mr-3"></i> + 54 379 4661 1164</p>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4 mx-auto mt-1">
+          <h5>Mantente Conectado</h5>
+          <p>Sigue a Smart Home Corrientes:</p>
+          <ul class="list-inline">
+            <li class="list-inline-item"><a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-google"></i></a></li>
+            <li class="list-inline-item"><a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-instagram"></i></a></li>
+
+          </ul>
+
+        </div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-6">
+          <p>&copy; 2023 Smart Home Corrientes. Todos los Derechos Reservados.</p>
+        </div>
+        <!-- Grid column -->
+        <div class="col-md-6">
+          <ul class="list-inline float-md-right">
+            <li class="list-inline-item"><a href="#">Politicas de Privacidad</a></li>
+            <li class="list-inline-item"><a href="#">Terminos y Condiciones</a></li>
+            <li class="list-inline-item"><a href="#">Boton de Arrepentimiento</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
 
   <!-- Bootstrap JS-->
   <script src="assets/js/bootstrap.bundle.min.js"></script>
