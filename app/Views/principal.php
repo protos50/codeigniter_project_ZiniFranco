5055fccb14ac0691840e8cde98cc0a499290a62b
protos50 <francojzini@gmail.com>
2023-04-24 22:37:53 -0300

    principal finished

--- app/Views/principal.php
+++ app/Views/principal.php
@@ -87,13 +87,14 @@
           </div>
         </div>
 
+        <!-- que son los productos inteligentes -->
         <div class="separador separador-grande mt-5 d-flex justify-content-center align-items-center">
           <h4 class="font-lato p-3">¿Pero qué son los productos inteligentes para el hogar?</h4>
         </div>
         <div class="d-flex justify-content-center align-items-center">
           <img class="mt-5 mb-3 p-md-5 pb-3 d-block h-100 img-fluid" src="assets/img/smart-devices.jpg" alt="">
         </div>
-        <div class="row d-flex justify-content-center align-items-center bordered-box-container bordered-box" style="background-image: url(assets/img/abstracto-naranja-azul.avif); box-shadow: -1px 4px 43px -2px rgb(102, 74, 55);">
+        <div class="mb-5 row d-flex justify-content-center align-items-center bordered-box-container bordered-box" style="background-image: url(assets/img/abstracto-naranja-azul.avif); box-shadow: -1px 4px 43px -2px rgb(102, 74, 55);">
           <div class="col-8 offset-2 col-md-6 offset-md-3 bordered-box" style="background-color: #0208bf39;box-shadow: -1px 4px 43px -2px rgb(102, 74, 55);">
             <p class="font-lato lead" style="color: black;text-align: justify;">Estos productos suelen ser controlados a través de una aplicación móvil o de voz, lo que permite a los usuarios ajustar su hogar desde cualquier lugar y en cualquier momento.</p>
             <p class="font-lato lead" style="color: black;text-align: justify;">Los productos inteligentes para el hogar son dispositivos conectados a Internet que permiten a los usuarios controlar y automatizar diferentes aspectos de su hogar, como la iluminación, la temperatura, la seguridad y los electrodomésticos. </p>
@@ -101,18 +102,92 @@
           </div>
         </div>
 
-        <!-- test -->
+
+        <!-- que ofrecemos -->
+
+        <div class=" card justify-content-center box-shadow" style="background-color: #0208bf39;">
+          <div class="row g-0">
+
+            <div class="col-lg-12 col-xl-6">
+              <img src="assets/img/smart-home.jpg" alt="Smart Home Product" class="img-fluid w-100 h-100">
+            </div>
+
+            <div class="my-auto col-lg-12 col-xl-6 h-100  d-flex justify-content-center align-items-center">
+              <div class="card-body" style=" text-align: justify;">
+                <h3 class="card-title font-lato text-center" style="font-size: max(2vw, 24px); font-weight: 900;">¿Qué ofrecemos?</h3>
+                <p class="font-poppins lead">Nuestra empresa ofrece productos inteligentes para el hogar que transforman su casa en un espacio conectado e inteligente. Con nuestra amplia gama de productos innovadores, nuestros clientes pueden mejorar la calidad de vida, ahorrar energía y aumentar la seguridad en sus hogares. Desde iluminación inteligente y termostatos inteligentes hasta sistemas de seguridad y cámaras de vigilancia, ofrecemos soluciones fáciles de usar que se integran perfectamente en la vida cotidiana de las personas.</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
 
 
 
-
-        <!-- test -->
         <div class="separador"></div>
+
+
+
       </div>
 
+      <!-- Amazon alexa -->
+      <div class="my-5 bg-color-2 box-shadow">
+        <div class="row ">
+          <div class="col-lg-12 col-xl-3" >
+
+    
+            <h4 class="text-center m-2 text-danger-emphasis fw-bold text-center font-raleway" style="font-size: max(2vw, 24px);">El asistente de voz que hace más que hablar</h4>
+            <p class="card-text font-notoSans m-3 lead" style="text-align: justify; font-size: max(1.2vw, 16px);">Alexa es un asistente de voz inteligente desarrollado por Amazon, que puede realizar diversas tareas mediante
+              comandos de voz. Puede ayudarte a reproducir música, establecer recordatorios, hacer llamadas, controlar dispositivos inteligentes en tu hogar, responder preguntas
+              y mucho más. También se puede conectar a otros servicios y aplicaciones para ampliar sus funcionalidades.</p>
+
+          </div>
+          <div class="col-lg-12 col-xl-9">
+            <div class="row g-2 h-100">
+
+              <div class="col-lg-7">
+                <div class="h-100">
+                  <img src="assets/img/alexa.jpg" class="img-fluid w-100 h-100" alt="...">
+                </div>
+              </div>
+
+              <div class="col-lg-5 ">
+                <div class="h-100">
+                  <img src="assets/img/alexa-2.jpg" class="img-fluid w-100 h-100" style="object-fit: cover;" alt="...">
+
+                </div>
+              </div>
+
+            </div>
+          </div>
 
 
-      <div class="card m-3 justify-content-center box bg-color">
+        </div>
+
+      </div>
+
+      <hr class="hr hr-blurry my-5" style="color: #ff6700" />
+
+      <!--cards iluminacion -->
+      <div class="my-5 card justify-content-center">
+        <div class="row g-0 align-items-center iluminacion-inteligente ">
+          <div class="col-md-6 ">
+            <div class="card-body d-flex flex-column justify-content-center align-content-center h-100 p-card ">
+              <h5 class="card-title font-notoSans mb-4 text-center">Descubre la revolución en iluminación con nuestra nueva tecnología de Iluminación Inteligente</h5>
+              <p class="card-text font-notoSans text-center">Con su capacidad de ajustar automáticamente el brillo y la tonalidad de la luz según tus preferencias y necesidades, nunca antes habías experimentado tanta comodidad y personalización en la iluminación de tu hogar. Además, con su conectividad inalámbrica y control remoto, nunca ha sido tan fácil controlar la iluminación de tu hogar desde cualquier lugar. Descubre la revolución en iluminación que te espera con nuestra Tecnología de Iluminación Inteligente.</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <img src="assets/img/iluminacion-inteligente.webp" alt="Smart Home Product" class="img-fluid h-100">
+          </div>
+          
+        </div>
+      </div>
+
+      <hr class="hr hr-blurry my-5" style="color: #ff6700" />
+
+      <!--cards robot aspirador -->
+      <div class="card justify-content-center box bg-color">
         <div class="row g-0 h-100 align-items-center">
           <div class="col-md-6 h-100">
             <img src="assets/img/img6.webp" alt="Smart Home Product" class="img-fluid h-100">
@@ -126,6 +201,7 @@
         </div>
       </div>
 
+      <!-- cards imagenes -->
       <div class="my-5">
         <div class="row g-2">
           <div class="col-md-12 col-lg-9 pb-md-2 pb-lg-0">
@@ -133,13 +209,13 @@
 
               <div class="col-md-7 box">
                 <div class="h-100">
-                  <img src="assets/img/matebook-d-series-1.jpg" class="img-fluid h-100" alt="...">
+                  <img src="assets/img/smart-products.webp" class="img-fluid h-100" style="object-fit: cover;" alt="...">
                 </div>
               </div>
 
               <div class="col-md-5 box">
                 <div class="h-100">
-                  <img src="assets/img/matebook-series-2.jpg" class="img-fluid h-100" alt="...">
+                  <img src="assets/img/yaya.jpg" class="img-fluid h-100" style="object-fit: cover;" alt="...">
 
                 </div>
               </div>
@@ -149,11 +225,11 @@
 
           <div class="col-md-12 col-lg-3">
 
-            <div class="row row-cols-lg-1 g-lg-3 g-md-2">
+            <div class="row row-cols-lg-1 g-2">
 
               <div class="col-md-6 box">
                 <div class="h-100">
-                  <img src="assets/img/img6.webp" class="img-fluid h-100" alt="...">
+                  <img src="assets/img/seguridad-2.jpg" class="img-fluid h-100" style="object-fit: cover;" alt="...">
 
                 </div>
               </div>
@@ -162,7 +238,7 @@
               <div class="col-md-6 box">
 
                 <div class="h-100">
-                  <img src="assets/img/img6.webp" class="img-fluid h-100" alt="...">
+                  <img src="assets/img/bulb-smart.jpg" class="img-fluid h-100" style="object-fit: cover;" alt="...">
 
                 </div>
               </div>
