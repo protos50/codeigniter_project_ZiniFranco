63d3b9461915030973e5d93ad6ff3dd72b6c15de
protos50 <francojzini@gmail.com>
2023-04-24 23:32:47 -0300

    titulos dinamicos

--- app/Views/header.php
+++ app/Views/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>Smart Home Corrientes</title>
+  <title><?=  $title ?></title>
 
 
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
